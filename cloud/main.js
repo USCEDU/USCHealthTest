b29851abe99a0c40b004e70cf7d3c2f8c3f52c3e
zhangzhe <zhangzhe@unisound.com>
2015-07-07 13:54:01 +0800

    1.Add object test

--- cloud/main.js
+++ cloud/main.js
@@ -4,5 +4,21 @@ applicationId= "";
 applicationKey="";
 
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  console.log("console test");
+  //response.success("Hello world!");
+  //response.success("Hello world!");
+  var MyTest = AV.Object.extend("test1");
+  //response.success("start save");
+  var test = new MyTest();
+  test.set("1","test1");
+
+  test.save(null, {
+    success:function(){
+      response.success("save success")
+    },
+    error:function (){
+      response.success("save error", + error.message);
+    }
+  })
+
 });
